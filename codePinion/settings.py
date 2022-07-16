50aad731ebd5470db45cd3ac3a86274d1551a639
The Meta <alexmeta517@gmail.com>
2022-07-16 11:33:33 +0300

    Change Allowed Hosts

--- codePinion/settings.py
+++ codePinion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a5l*h3exp)t%6(=sx++5h%fo_xnlp7x3lhoxhoqil_!@1hcmub
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["codepinion.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
