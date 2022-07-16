e280e0556817caa67e9086a898096d18ccec88ef
The Meta <alexmeta517@gmail.com>
2022-07-16 11:16:30 +0300

    Change codeSage to codePinion

--- codePinion/settings.py
+++ codePinion/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'codeSage.urls'
+ROOT_URLCONF = 'codePinion.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'codeSage.wsgi.application'
+WSGI_APPLICATION = 'codePinion.wsgi.application'
 
 
 # Database
